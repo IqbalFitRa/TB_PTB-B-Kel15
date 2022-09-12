a478c5545c97eee37c23ac77bcaad71da7603050
IqbalFitRa <89772053+IqbalFitRa@users.noreply.github.com>
2022-09-12 10:20:51 +0700

    Initial commit

---
+++ app/src/main/java/com/kelompok_15/tb_ptb_b/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kelompok_15.tb_ptb_b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
